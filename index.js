ea68882b5ccadfaff9301e20a4d0342d42d644eb
Anna Kolliakou <akolliakou@gmail.com>
2022-03-29 20:02:15 +0100

    update backend

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 app.use(morgan((tokens, req, res) => {
   return [
     tokens.method(req, res),
